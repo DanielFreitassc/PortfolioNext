f9e3be94041b93ef11a0640c4f1dccf3ecaf4b3a
DanielFreitassc <danielfreitassc@gmail.com>
2025-05-27 23:13:17 -0300

    Made routes with App Router and added a few components ⚙️⚙️

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header"
+
+export default function Home() {
+  return (
+    <div className="bg-">
+      <Header/>
+      <h1>Home</h1>
+      <Footer/>
+    </div>
+  );
+}
