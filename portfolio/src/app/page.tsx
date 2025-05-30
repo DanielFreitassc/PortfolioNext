98f8ef3ee11f14fa0a915678336c97ebe7a4fa45
DanielFreitassc <danielfreitassc@gmail.com>
2025-05-30 17:55:37 -0300

    Feat: Trabalhando responsividade

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
           <h1 className="text-[3.6rem] max-[999px]:text-[1.5rem]">
             Daniel é um <span className="text-[#C778DD]">Dev Backend</span> com Experiência na área <span className="text-[#C778DD]">DevSecOps</span>
           </h1>
-          <h2 className="text-2xl max-[999px]:text-[1rem]text-[#ABB2BF]">
+          <h2 className="text-2xl max-[999px]:text-[1rem] text-[#ABB2BF]">
             Dedicado, focado em construir APIs escaláveis e soluções seguras.
           </h2>
-          <button className="my-10 p-1 w-50 border-[1px] border-[#C778DD] text-[1.2rem]">
+          <button className="my-10 p-1 w-50 border-[1px] border-[#C778DD] text-[1.2rem] hover:bg-[rgba(199,120,221,0.1)] cursor-pointer">
             Contate-me !
           </button>
         </div>
@@ -26,11 +26,12 @@ export default function Home() {
           <Image 
             src="/daniel.png" 
             alt="Foto daniel"
-            width={400}
+            width={0}
             height={0}
-            className="w-full h-auto"
+            sizes="100vw"
+            className="w-[450px] max-[999px]:w-[300px] h-auto mr-15 max-[999px]:mr-0"
           />
-          <div className="flex items-center justify-center text-[1.2rem] max-[999px]:text-[1rem] text-[#ABB2BF] mx-10 max-[999px]:mx-1 gap-x-2 p-1.5 border-[1px] border-brand-gray">
+          <div className="flex w-130 max-[999px]:w-[100%] items-center justify-center text-[1.2rem] max-[999px]:text-[0.8rem] text-[#ABB2BF] mx-10 gap-x-2 p-[0.2rem] border-[1px] border-brand-gray">
             <div className="relative h-4 w-4">
               <Image
                 src="/Rectangle.png"
