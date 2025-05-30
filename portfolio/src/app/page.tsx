0c63b63e356340acaf7d007d73d1de342338677c
DanielFreitassc <danielfreitassc@gmail.com>
2025-05-29 23:52:50 -0300

    Feat: mobile

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -8,22 +8,39 @@ export default function Home() {
       <Header/>
 
       {/* Container divs texto e foto */}
-      <div className="flex px-44 py-30 justify-between items-center">
+      <div className="flex flex-col lg:flex-row px-25 max-[999px]:px-5 py-10 lg:py-30 justify-between items-center">
         {/* Container texto */}
-        <div className="flex-column pr-50">
-          <h1 className="text-5xl">Daniel é um <span className="text-[#C778DD]">Dev Backend</span> com Experiência na área <span className="text-[#C778DD]">DevSecOps</span></h1>
-          <h2 className="text-2xl text-[#ABB2BF]">Dedicado, focado em construir APIs escaláveis e soluções seguras.</h2>
-          <button className="p-1 border-[1px] border-[#C778DD] text-2xl">Contate-me !!</button>
+        <div className="flex flex-col pr-12 max-[999px]:pr-0 max-w-[50%] max-[999px]:max-w-[100%]">
+          <h1 className="text-[3.6rem] max-[999px]:text-[1.5rem]">
+            Daniel é um <span className="text-[#C778DD]">Dev Backend</span> com Experiência na área <span className="text-[#C778DD]">DevSecOps</span>
+          </h1>
+          <h2 className="text-2xl max-[999px]:text-[1rem]text-[#ABB2BF]">
+            Dedicado, focado em construir APIs escaláveis e soluções seguras.
+          </h2>
+          <button className="my-10 p-1 w-50 border-[1px] border-[#C778DD] text-[1.2rem]">
+            Contate-me !
+          </button>
         </div>
-        {/* Container foto minha */}
-        <div className="">
+        {/* Container imagem*/}
+        <div className="flex flex-col items-center max-w-[50%] max-[999px]:max-w-[100%]">
           <Image 
             src="/daniel.png" 
             alt="Foto daniel"
-            width={900}
+            width={400}
             height={0}
+            className="w-full h-auto"
           />
-
+          <div className="flex items-center justify-center text-[1.2rem] max-[999px]:text-[1rem] text-[#ABB2BF] mx-10 max-[999px]:mx-1 gap-x-2 p-1.5 border-[1px] border-brand-gray">
+            <div className="relative h-4 w-4">
+              <Image
+                src="/Rectangle.png"
+                alt="Foto de quadrado"
+                fill
+                className="object-contain"
+              />
+            </div>
+            Atuamente trabalhando em <span className="text-[#C778DD]">CSIRT/SATC</span>
+          </div>
         </div>
       </div>
       <Footer/>
